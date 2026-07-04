440220af3bf1b910b83094252cd7c48a61b45c05
oscarbprofe <oscar.badilla@profesor.duoc.cl>
2026-07-03 20:02:22 -0400

    crea función para menú y para opción uno contador de pergaminos

--- et_oscar_badilla.py
+++ et_oscar_badilla.py
@@ -22,3 +22,58 @@ reservas = {
  'H005': [210, 5],
  'H006': [450, 4],
 }
+
+def leer_opcion():
+
+    print('''
+    ========= GRIMORIO ASTRALIS =========
+    1. Pergaminos por escuela de magia
+    2. Búsqueda de hechizos por rango de precio
+    3. Actualizar precio de hechizo
+    4. Agregar hechizo
+    5. Eliminar hechizo
+    6. Salir
+    =====================================
+    ''')
+    try:
+        eleccion = int(input("Ingrese una opción: "))
+        if eleccion < 1 or eleccion > 6:
+            print("Debe seleccionar una opción válida")
+            return 0
+        return eleccion
+    except ValueError:
+        print("Debe seleccionar una opción válida")
+        return 0
+
+def pergaminos_escuela(escuela, hechizos, reservas):
+    print(f"Pergaminos disponibles para la escuela de magia '{escuela}':")
+    cont_pergaminos_escuela = 0
+    
+    for codigo, datos in hechizos.items():
+        if datos[1].lower() == escuela.lower():
+            cont_pergaminos_escuela += reservas[codigo][1]
+    
+    print(f"Total de pergaminos para la escuela '{escuela}': {cont_pergaminos_escuela}")
+    
+
+while True:
+    opcion = leer_opcion()
+    
+    if opcion == 1:
+        escuela = input("Ingrese la escuela de magia: ")
+        pergaminos_escuela(escuela, hechizos, reservas)
+    elif opcion == 2:
+        # Implementar búsqueda de hechizos por rango de precio
+        pass
+    elif opcion == 3:
+        # Implementar actualización de precio de hechizo
+        pass
+    elif opcion == 4:
+        # Implementar agregar hechizo
+        pass
+    elif opcion == 5:
+        # Implementar eliminar hechizo
+        pass
+    elif opcion == 6:
+        print("Saliendo del programa...")
+        break
